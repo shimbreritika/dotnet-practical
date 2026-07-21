34058117890924c8c710d679a1075ba23febf96a
Ritika Shimbre <shimbreritika@gmail.com>
2026-07-21 20:23:32 +0530

    Add files via upload

---
+++ ASSIGNMENT/Assignment9/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment9.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+        public string Email { get; set; }
+    }
+}
