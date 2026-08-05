24df2a413cad963043cbf868028a8d2ba7cc2268
Ritika Shimbre <shimbreritika@gmail.com>
2026-08-05 09:48:55 +0530

    Add files via upload

---
+++ ASSIGNMENT/Assignment18/Service/CourseService.cs
@@ -0,0 +1,49 @@
+﻿using Assignment18.Data;
+using Assignment18.Model;
+using Assignment18.Respository;
+
+namespace Assignment18.Service
+{
+    public class CourseService : ICourseService
+    {
+        private readonly AppDbContext context;
+
+        public CourseService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void AddCourse(Course course)
+        {
+            context.Courses.Add(course);
+            context.SaveChanges();
+        }
+
+        public void DeleteCourse(int id)
+        {
+            var course = context.Courses.Find(id);
+
+            if (course != null)
+            {
+                context.Courses.Remove(course);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Course> GetAll()
+        {
+            return context.Courses.ToList();
+        }
+
+        public Course? GetCourse(int id)
+        {
+            return context.Courses.Find(id);
+        }
+
+        public void UpdateCourse(Course course)
+        {
+            context.Courses.Update(course);
+            context.SaveChanges();
+        }
+    }
+}
