98b2152432e7bba8ee13703d875809ef98629131
Ritika Shimbre <shimbreritika@gmail.com>
2026-08-18 00:17:17 +0530

    Add files via upload

---
+++ ASSIGNMENT/Assignment_13JUL/Customer.cs
@@ -0,0 +1,7 @@
+class Customer
+{
+    public int CustomerId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ ASSIGNMENT/Assignment_13JUL/Product.cs
@@ -0,0 +1,7 @@
+class Product
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public double Price { get; set; }
+    public int Stock { get; set; }
+}
