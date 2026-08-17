6fb5e95cc9a7c66794cb18351c0f5d5c56349032
Ritika Shimbre <shimbreritika@gmail.com>
2026-08-18 00:07:35 +0530

    Delete ASSIGNMENT/Assignment13 directory

--- ASSIGNMENT/Assignment13/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Assignment13.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
